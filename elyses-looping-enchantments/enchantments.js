33079c58c24b15eefa529279c6607e3dc69c86f4
Richard Leer <github@rleer.de>
2026-03-08 17:10:37 +0100

    add elyses-looping-enchantments exercise

---
+++ elyses-looping-enchantments/enchantments.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  // 🚨 Use .forEach
+  let count = 0;
+  stack.forEach(value => {
+    count += value === card ? 1 : 0;
+  });
+  return count;
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  // 🚨 Use a `for...of` loop
+  let count = 0;
+  const rest = type ? 0 : 1;
+  for (const card of stack) {
+    if (card % 2 === rest) {
+      count++;
+    }
+  }
+  return count;
+}
